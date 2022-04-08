6a4f2509ddddab0e6316250c0a454be1c7b17dc2
anupam41 <anupamsamanta41@gmail.com>
2022-04-08 23:06:09 +0000

    Settings/AboutPhone/Aboutus : Aboutus
    Settings/AboutPhone : Dissolve and convert about phone to AboutUS
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Change-Id: I79428e6542d5f7ad633acc9fb2bfebf4b2376270
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Update blaze_styles.xml
    we will not load this as it force closes on a few devices this has been reported by the testing group
    Co-Authored-By: mezz <mezzlasha@gmail.com>
    Co-Authored-By: imjyotiraditya <jyotiraditya@aospa.co>
    Co-Authored-By: Maitreya29 <maitreyapatni30@gmail.com>

--- src/com/radiant/widget/WallpaperBlurView.java
+++ src/com/radiant/widget/WallpaperBlurView.java
@@ -14,32 +14,37 @@
  * limitations under the License.
  */
 
-package com.afterlife.preferences;
+package com.radiant.widget;
 
 import android.app.WallpaperManager;
 import android.content.Context;
+import android.graphics.RenderEffect;
+import android.graphics.Shader;
 import android.util.AttributeSet;
 import android.widget.ImageView;
 
 
-public class aboutWallpaperView extends ImageView {
+public class WallpaperBlurView extends ImageView {
 
     Context contextM;
 
-    public aboutWallpaperView(Context context, AttributeSet attrs, int defStyleAttr) {
+    public WallpaperBlurView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         contextM = context;
+        setRenderEffect(RenderEffect.createBlurEffect(15, 15, Shader.TileMode.CLAMP));
     }
 
-    public aboutWallpaperView(Context context, AttributeSet attrs) {
+    public WallpaperBlurView(Context context, AttributeSet attrs) {
         super(context, attrs);
         contextM = context;
+        setRenderEffect(RenderEffect.createBlurEffect(15, 15, Shader.TileMode.CLAMP));
     }
 
 
-    public aboutWallpaperView(Context context) {
+    public WallpaperBlurView(Context context) {
         super(context);
         contextM = context;
+        setRenderEffect(RenderEffect.createBlurEffect(15, 15, Shader.TileMode.CLAMP));
     }
 
     @Override
