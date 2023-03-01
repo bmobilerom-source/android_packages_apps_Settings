286c9ffc9be57d0818c500eb896c9805728845d7
the projects <84352313+theorchidproject@users.noreply.github.com>
2023-03-01 02:28:58 +0000

    LottieFiles : Import LottieFiles
    LottieFiles takes away the complexity from Motion Design.
    A Lottie is a JSON-based animation file format that allows you to ship animations on any platform as easily as shipping static assets. They are small files that work on any device and can scale up or down without pixelation. LottieFiles lets you create, edit, test, collaborate on and ship a Lottie in the easiest way possible.
    thank you to all designers for free files
    https://lottiefiles.com/
    
    SystemUI/Headers : Import headers for system
    
    .
    
    Co-Authored-By: str-br <alexfinhart@gmail.com>
    Co-Authored-By: ghostrider-reborn <gh0strider.2k18.reborn@gmail.com>
    Co-Authored-By: IacobIonut01 <paulionut2003@gmail.com>
    
    Settings/Lottie : Import illustrations
    
    Imported Motorola Illustrations
    Integrated clean, minimal illustrations from Motorola to improve UI polish and feature onboarding screens.
    
    Location: settings illustrations module / drawable or lottie assets
    
    Change-Id: I62d4f860ed3763e3fc051737d15c1770fde1afe6
    
    Signed-off-by: minaripenguin minaripenguin@users.noreply.github.com
    
    Updated Illustrations from LottieFiles
    Replaced or enhanced existing animations with newer ones from LottieFiles for smoother UX.
    
    Targets a more modern visual identity for Settings pages.
    
    Fix: Illustration Overlap in Location Settings
    UI bugfix: Resolved overlap between Lottie animation and content in Location Settings screen.
    
    Signed-off-by: Pranav Vashi neobuddy89@gmail.com
    
    SeekBarDialogPreference: Summary TextView Added
    Enhancement: Added a TextView for summary to SeekBarDialogPreference.
    
    Gives users contextual info below the slider (what the value controls or units).
    
    Useful for audio, brightness, custom system value sliders.
    
    Change-Id: Icff728749c3e597a4127d77646bdf3a742c35d83
    
    Contributors:
    
    cjybyjk cjybyjk@zjnu.edu.cn
    
    Dmitrii bankersenator@gmail.com
    
    Pranav Vashi neobuddy89@gmail.com
    
    System UI: Header Imports
    Imported new system headers to match Settings branding and aesthetics.
    
    Linked to dynamic headers/accent-colored titles from OrchidOS work.
    
    Ensures visual consistency across Settings & System UI.
    
    Settings: Add missing animations
    Change-Id: I1a566a78e0afd6b024d2fc911f2f0233c8250a42
    
    Signed-off-by: Jason Edson jaysonedson@gmail.com
    
    Signed-off-by: SagarMakhar sagarmakhar@gmail.com
    
    Settings: Bring in missing lottie animations
    Extracted from redfin b5 SettingsGoogle
    
    Change-Id: Ifc81d60c1ab00caf6bf5b5242656e1d2c8186e3f
    
    Settings: Import SettingsGoogle animations from UP1A.231005.007.A1
    Change-Id: I004388394c38a908981e4996137b3db9f622345c
    
    Signed-off-by: minaripenguin minaripenguin@users.noreply.github.com
    
    Settings: Add a preference to battery optimization page
    New preference lets users open a page listing all apps for battery optimization, with filters.
    
    Change-Id: I4bca6faccc489c3a48d3dfb4509cd0924a825566
    
    Signed-off-by: jhonboy121 alfredmathew05@gmail.com
    
    [marshmello61] add missing summary for visual consistency
    
    Signed-off-by: Mayur ultramayur123@gmail.com
    
    Signed-off-by: aswin7469 aswinas@pixysos.com
    
    OrchidOS Devices : Config Updates
    Emergency info tile now displays on device info page.
    
    Option to show mobile plan preference item.
    
    Enable color temperature setting.
    
    Updated config.xml.
    
    Co-Authored-By: xyzuan <xyzuannihboss@gmail.com>
    Co-Authored-By: beingmishra <rahul731999@gmail.com>
    Co-Authored-By: minaripenguin <minaripenguin@users.noreply.github.com>

--- src/com/android/settings/display/darkmode/DarkModeActivationPreferenceController.java
+++ src/com/android/settings/display/darkmode/DarkModeActivationPreferenceController.java
@@ -27,6 +27,7 @@ import android.widget.Button;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceScreen;
 
+import com.android.settings.R;
 import com.android.settings.core.BasePreferenceController;
 import com.android.settings.overlay.FeatureFactory;
 import com.android.settingslib.core.instrumentation.MetricsFeatureProvider;
