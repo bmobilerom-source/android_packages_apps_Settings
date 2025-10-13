155361efd3cf41bac991bc522030853881e7d567
The-projects <simplncprivate@gmail.com>
2025-10-13 02:52:47 +0100

    Dashboard-Settings : Settings Dashboard Updates and Introduce Welcome Da…

--- src/com/android/settings/homepage/TopLevelSettings.java
+++ src/com/android/settings/homepage/TopLevelSettings.java
@@ -34,7 +34,9 @@ import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.UserManager;
 import android.provider.Settings;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.VisibleForTesting;
