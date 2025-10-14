730337a88a5c128d5f3a00ea551f46085997c0f7
theJecks <simplncprivate@gmail.com>
2025-10-14 22:18:26 +0100

    Update UserInfoFragement.java

--- src/com/android/settings/deviceinfo/UserInfoFragement.java
+++ src/com/android/settings/deviceinfo/UserInfoFragement.java
@@ -85,7 +85,7 @@ public class UserInfoFragement extends SettingsPreferenceFragment {
 
     @Override
     public int getMetricsCategory() {
-        return SettingsEnums.BASHA_SETTINGS;
+        return SettingsEnums.BASHA;
     }
 
    static String getEmail(Context context) {
