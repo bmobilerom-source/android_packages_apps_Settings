24c791572123c9b9e3fb807b4b1fdccabd798d13
The-projects <simplncprivate@gmail.com>
2025-10-26 15:32:26 +0000

    [2/2] B-mobile : On-The-Go Mode
    [2/2] : B-mobile : On-The-Go Mode
    On-The-Go Mode :
    Real-time Transparency: Slider changes apply immediately to camera overlay
    Camera Switching: Toggle between front/rear camera with service restart
    Device Awareness: Options only show when hardware supports them
    Broadcast Communication: Uses Intent broadcasts to communicate with service
    Settings Persistence: All settings save and restore properly
    How It Works
    Transparency: Adjusts ON_THE_GO_ALPHA setting and sends broadcast to service
    Camera: Changes ON_THE_GO_CAMERA setting and triggers service restart
    Service Restart: Controls ON_THE_GO_SERVICE_RESTART for automatic behavior
    
    System/Display : [2/2] : B-mobile : On-The-Go Mode

---
+++ src/com/android/settings/power/OnTheGoAlphaPreferenceController.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright (C) 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.power;
+
+import android.content.Context;
+import android.content.Intent;
+import android.provider.Settings;
+import android.util.Log;
+
+import com.android.settings.core.SliderPreferenceController;
+import com.android.settings.widget.SeekBarPreference;
+import com.android.settingslib.core.AbstractPreferenceController;
+
+/**
+ * Controller for OnTheGo transparency preference
+ */
+public class OnTheGoAlphaPreferenceController extends SliderPreferenceController {
+
+    private static final String TAG = "OnTheGoAlphaController";
+    private static final String KEY_ONTHEGO_ALPHA = "onthego_alpha";
+    private static final String SETTINGS_KEY_ONTHEGO_ALPHA = "on_the_go_alpha";
+
+    public OnTheGoAlphaPreferenceController(Context context) {
+        super(context, KEY_ONTHEGO_ALPHA);
+    }
+
+    @Override
+    public int getAvailabilityStatus() {
+        return AVAILABLE;
+    }
+
+    @Override
+    public int getSliderPosition() {
+        float alpha = Settings.System.getFloat(mContext.getContentResolver(),
+                SETTINGS_KEY_ONTHEGO_ALPHA, 0.5f);
+        return (int) (alpha * 100);
+    }
+
+    @Override
+    public boolean setSliderPosition(int position) {
+        float alpha = position / 100.0f;
+        Settings.System.putFloat(mContext.getContentResolver(),
+                SETTINGS_KEY_ONTHEGO_ALPHA, alpha);
+        
+        // Send broadcast to update the service
+        sendAlphaBroadcast(alpha);
+        return true;
+    }
+
+    @Override
+    public int getMax() {
+        return 75;
+    }
+
+    @Override
+    public int getMin() {
+        return 10;
+    }
+
+    private void sendAlphaBroadcast(float alpha) {
+        try {
+            Intent alphaBroadcast = new Intent("toggle_alpha");
+            alphaBroadcast.putExtra("extra_alpha", alpha);
+            mContext.sendBroadcast(alphaBroadcast);
+            Log.e(TAG, "Sent alpha broadcast: " + alpha);
+        } catch (Exception e) {
+            Log.e(TAG, "Error sending alpha broadcast: " + e.getMessage());
+        }
+    }
+}
