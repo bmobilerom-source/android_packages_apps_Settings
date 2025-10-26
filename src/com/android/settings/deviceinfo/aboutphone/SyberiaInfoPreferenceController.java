21eeffe9b8a609935ffeffbdb86f419ed024ee1f
The-projects <simplncprivate@gmail.com>
2025-10-26 16:59:18 +0000

    tests can revert

--- src/com/android/settings/deviceinfo/aboutphone/SyberiaInfoPreferenceController.java
+++ src/com/android/settings/deviceinfo/aboutphone/SyberiaInfoPreferenceController.java
@@ -21,6 +21,7 @@ import java.io.IOException;
 import android.content.Context;
 import android.os.SystemProperties;
 import android.widget.TextView;
+import android.util.Log;
 import androidx.preference.PreferenceFragmentCompat;
 import androidx.preference.PreferenceScreen;
 
@@ -35,6 +36,7 @@ import com.android.settings.core.PreferenceControllerMixin;
 
 public class SyberiaInfoPreferenceController extends AbstractPreferenceController {
 
+    private static final String TAG = "SyberiaInfoPrefCtrl";
     private static final String KEY_SYBERIA_INFO = "syberia_info";
 
     public SyberiaInfoPreferenceController(Context context) {
@@ -44,15 +46,51 @@ public class SyberiaInfoPreferenceController extends AbstractPreferenceControlle
     @Override
     public void displayPreference(PreferenceScreen screen) {
         super.displayPreference(screen);
-        final LayoutPreference syberiaInfoPreference = screen.findPreference(KEY_SYBERIA_INFO);
-        final TextView processor = (TextView) syberiaInfoPreference.findViewById(R.id.processor_message);
-        final TextView storage = (TextView) syberiaInfoPreference.findViewById(R.id.storage_code_message);
-        final TextView battery = (TextView) syberiaInfoPreference.findViewById(R.id.battery_type_message);
-        final TextView infoScreen = (TextView) syberiaInfoPreference.findViewById(R.id.screen_message);
-        processor.setText(SyberiaSpecUtils.getProcessorModel());
-        storage.setText(String.valueOf(SyberiaSpecUtils.getTotalInternalMemorySize()) + "GB ROM + " + String.valueOf(SyberiaSpecUtils.getTotalRAM()) + "GB RAM");
-        battery.setText(SyberiaSpecUtils.getBatteryCapacity(mContext) + " mAh");
-        infoScreen.setText(SyberiaSpecUtils.getScreenRes(mContext));
+        try {
+            final LayoutPreference syberiaInfoPreference = screen.findPreference(KEY_SYBERIA_INFO);
+            if (syberiaInfoPreference == null) {
+                return;
+            }
+            
+            final TextView processor = (TextView) syberiaInfoPreference.findViewById(R.id.processor_message);
+            final TextView storage = (TextView) syberiaInfoPreference.findViewById(R.id.storage_code_message);
+            final TextView battery = (TextView) syberiaInfoPreference.findViewById(R.id.battery_type_message);
+            final TextView infoScreen = (TextView) syberiaInfoPreference.findViewById(R.id.screen_message);
+            
+            if (processor != null) {
+                try {
+                    processor.setText(SyberiaSpecUtils.getProcessorModel());
+                } catch (Exception e) {
+                    processor.setText("Unknown");
+                }
+            }
+            
+            if (storage != null) {
+                try {
+                    storage.setText(String.valueOf(SyberiaSpecUtils.getTotalInternalMemorySize()) + "GB ROM + " + String.valueOf(SyberiaSpecUtils.getTotalRAM()) + "GB RAM");
+                } catch (Exception e) {
+                    storage.setText("Unknown");
+                }
+            }
+            
+            if (battery != null) {
+                try {
+                    battery.setText(SyberiaSpecUtils.getBatteryCapacity(mContext) + " mAh");
+                } catch (Exception e) {
+                    battery.setText("Unknown");
+                }
+            }
+            
+            if (infoScreen != null) {
+                try {
+                    infoScreen.setText(SyberiaSpecUtils.getScreenRes(mContext));
+                } catch (Exception e) {
+                    infoScreen.setText("Unknown");
+                }
+            }
+        } catch (Exception e) {
+            // Silently fail to prevent crashes
+        }
     }
 
     @Override
