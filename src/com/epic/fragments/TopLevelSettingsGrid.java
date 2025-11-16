503746834efd7eff0bcc04063a11112d79b0b89d
theJecks <simplncprivate@gmail.com>
2025-11-16 21:12:24 +0000

    [2/2] SYSTEM-DASHBOARD : Custom settings dashboard
    SYSTEM-DASHBOARD : Custom settings dashboard

--- src/com/epic/fragments/TopLevelSettingsGrid.java
+++ src/com/epic/fragments/TopLevelSettingsGrid.java
@@ -133,7 +133,7 @@ public class TopLevelSettingsGrid extends SettingsPreferenceFragment implements
                     R.string.display_grid_lock_title,
                     R.string.display_grid_lock_summary,
                     R.drawable.ic_display_grid_lock,
-                    "com.android.settings.security.SecuritySettings"));
+                    "com.epic.fragments.SettingsExtendedSecurity"));
             
             items.add(new TopLevelSettingsGridAdapter.CardItem(
                     TopLevelSettingsGridAdapter.CARD_TYPE_CIRCULAR_BUTTON,
