576deee81473edbd8a9814cb980affbd26c19d07
theJecks <simplncprivate@gmail.com>
2025-11-22 05:23:20 +0000

    [2/2] QS : Add QS Header Image customization

--- src/com/android/settings/awaken/fragments/QsHeader.java
+++ src/com/android/settings/awaken/fragments/QsHeader.java
@@ -76,7 +76,7 @@ public class QsHeader extends SettingsPreferenceFragment implements OnPreference
         if (mHeaderVisibility != null) {
             mHeaderVisibility.setOnPreferenceChangeListener(this);
             int visibility = Settings.System.getIntForUser(resolver,
-                    Settings.System.STATUS_BAR_CUSTOM_HEADER, 0, UserHandle.USER_CURRENT);
+                    "qs_header_image_enabled", 1, UserHandle.USER_CURRENT);
             mHeaderVisibility.setValue(String.valueOf(visibility));
             updateVisibilitySummary(visibility);
         }
@@ -135,13 +135,13 @@ public class QsHeader extends SettingsPreferenceFragment implements OnPreference
         } else if (preference == mHeaderVisibility) {
             int visibility = Integer.parseInt((String) newValue);
             boolean success = Settings.System.putIntForUser(resolver,
-                    Settings.System.STATUS_BAR_CUSTOM_HEADER,
+                    "qs_header_image_enabled",
                     visibility, UserHandle.USER_CURRENT);
             if (success) {
                 // Notify SystemUI of the change
                 resolver.notifyChange(
-                        Settings.System.getUriFor(Settings.System.STATUS_BAR_CUSTOM_HEADER),
-                        null, false);
+                        Settings.System.getUriFor("qs_header_image_enabled"),
+                        null, true);
                 updateVisibilitySummary(visibility);
             }
             return success;
