99e2858bcf2f8bb0b9c108279d7a9451f9a9ac8c
theJecks <simplncprivate@gmail.com>
2025-12-01 23:17:00 +0000

    Settings/AppsUI : Enhance apps page
    add system search launcher add native allpha add digpaws apps

---
+++ src/com/android/settings/applications/NativeAlphaPreferenceController.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2025 LineageOS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.applications;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+
+import com.android.settings.core.BasePreferenceController;
+
+/**
+ * Controller for Native Alpha browser preference.
+ * Only shows the preference if the Native Alpha app is installed.
+ */
+public class NativeAlphaPreferenceController extends BasePreferenceController {
+
+    private static final String PACKAGE_NAME = "com.android.nativealpha";
+
+    public NativeAlphaPreferenceController(Context context, String preferenceKey) {
+        super(context, preferenceKey);
+    }
+
+    @Override
+    public int getAvailabilityStatus() {
+        return isAppInstalled() ? AVAILABLE : UNSUPPORTED_ON_DEVICE;
+    }
+
+    private boolean isAppInstalled() {
+        try {
+            mContext.getPackageManager().getPackageInfo(PACKAGE_NAME, 0);
+            return true;
+        } catch (PackageManager.NameNotFoundException e) {
+            return false;
+        }
+    }
+}
+
+
+
